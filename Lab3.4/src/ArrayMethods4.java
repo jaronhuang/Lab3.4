a9c796d3e904d2c9c8de71b7bf5e1330aa747700
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-28 12:19:47 -0500

    .

---
+++ Lab3.4/src/ArrayMethods4.java
@@ -0,0 +1,18 @@
+
+public class ArrayMethods4 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static void partition(int[] list1, int front, int back)
+	{
+		
+	}
+	
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		
+	}
+}
