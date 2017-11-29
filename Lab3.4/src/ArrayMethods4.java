27b3c1e49163681e3b61d9186788a155486d48b3
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-29 13:11:22 -0500

    .

--- Lab3.4/src/ArrayMethods4.java
+++ Lab3.4/src/ArrayMethods4.java
@@ -1,14 +1,33 @@
+import java.util.Arrays;
 
 public class ArrayMethods4 
 {
 	public static void main(String[] args)
 	{
-		
+		int[] list = {1, 7, 4, 3, 8, 2, 9};
+		partition(list, 2, 4);
+		printArray(list);
 	}
 	
 	public static void partition(int[] list1, int front, int back)
 	{
+		list1 = Arrays.copyOfRange(list1, front, back);
 		
+		/*int pivotPos = (back - front) / 2;
+		
+		for (int i = 0; i < pivotPos; i++)
+		{
+			if (list1[i] > list1[pivotPos])
+			{
+				for (int j = pivotPos + 1; j < list1.length; j++)
+				{
+					if (list1[j] < list1[pivotPos])
+					{
+						swap(list1, i, j);
+					}
+				}
+			}
+		}*/
 	}
 	
 	public static void quickSort(int[] list1, int front, int back)
@@ -16,16 +35,30 @@ public class ArrayMethods4
 		
 	}
 	
-	public static int findMedian(int[] list1, int front, int back)
+
+	/**
+	 * Swaps the position of two integers
+	 * @param arr Array of integers
+	 * @param i Index of the first integer you want to swap
+	 * @param j Index of the second integer you want to swap
+	 */
+	public static void swap(int[] arr, int i, int j)
 	{
-		int[] nList;
-		nList = new int[back - front];
-		int pos = 0;
-		for (int i = front; i < back; i++)
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	/**
+	 * Prints out each element of the array
+	 * @param arr Array of integers
+	 */
+	public static void printArray(int[] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
 		{
-			list1[i] = nList[pos];
-			pos++;
+			System.out.print(arr[i] + " ");
 		}
-		return 1;
+		System.out.println();
 	}
 }
