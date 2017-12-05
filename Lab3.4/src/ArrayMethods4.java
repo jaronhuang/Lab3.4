12dc2010b9a13a1e0bd73f732eef222a1524b646
Jaron <Jaron@Jaron.home>
2017-12-04 22:59:04 -0500

    .

--- Lab3.4/src/ArrayMethods4.java
+++ Lab3.4/src/ArrayMethods4.java
@@ -8,37 +8,49 @@ public class ArrayMethods4
 		partition(list, 2, 7);
 	}
 	
-	public static void partition(int[] list1, int front, int back)
+	public static int partition(int[] list1,int front,int back)
 	{
-		list1 = Arrays.copyOfRange(list1, front, back);
+		int pivotPos = (front + back)/2;
 		
-		int pivotPos = (back - front) / 2;
-		
-		printArray(list1);
-		
-		System.out.println("pivot position: " + pivotPos + " value: " + list1[pivotPos]);
-		
-		for (int i = pivotPos + 1; i < list1.length; i++)
+		boolean isPartition = false;
+		while(!isPartition)
 		{
-			if (list1[i] < list1[pivotPos])
+			isPartition = true;
+		
+			for(int i = 0; i < pivotPos; i++)
 			{
-				for (int j = i; j > pivotPos; j--)
+			
+				if(list1[i] > list1[pivotPos])
 				{
-					if (j - 1 == pivotPos)
-					{
-						swap(list1, i, pivotPos);
-						pivotPos = i;
-					}
-					if (list1[i] < list1[j])
-					{
-						swap(list1, i, j);
-					}
+					isPartition = false;
+					swap(list1, i, pivotPos);
+					pivotPos = i;
+	
+				}
+				if(i == pivotPos)
+				{
+					break;
 				}
 			}
 		}
-		
-		printArray(list1);
+		for(int j = back; j > pivotPos; j--)
+		{
+			
+			if(list1[j] < list1[pivotPos])
+			{
+				isPartition = false;
+				swap(list1, j, pivotPos);
+				pivotPos = j;
+
+			}
+			if(j == pivotPos)
+			{
+				break;
+			}
+		}
+		return pivotPos;
 	}
+
 	
 	public static void quickSort(int[] list1, int front, int back)
 	{
